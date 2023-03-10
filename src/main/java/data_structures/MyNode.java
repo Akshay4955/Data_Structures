cba0863e183376329abf4c8875fe49e75890ebe7
Akshay4955 <akshayshedge11@gmail.com>
2023-03-10 11:51:47 +0530

    Added: Code to create simple linked list

---
+++ src/main/java/data_structures/MyNode.java
@@ -0,0 +1,18 @@
+package data_structures;
+
+public class MyNode<K> {
+    private K key;
+    private MyNode next;
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+}
