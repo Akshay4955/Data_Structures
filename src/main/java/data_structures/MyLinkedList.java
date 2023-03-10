974212a6b7b98424b8dfe1c12a03cd513088ccce
Akshay4955 <akshayshedge11@gmail.com>
2023-03-10 17:04:57 +0530

    Added: Code to insert node in linked list

--- src/main/java/data_structures/MyLinkedList.java
+++ src/main/java/data_structures/MyLinkedList.java
@@ -10,11 +10,10 @@ public class MyLinkedList {
     }
 
     public void add(INode newNode) {
-        if(this.head == null) {
+        if (this.head == null) {
             this.head = newNode;
             this.tail = newNode;
-        }
-        else {
+        } else {
             INode tempNode = this.head;
             this.head = newNode;
             this.head.setNext(tempNode);
@@ -22,26 +21,35 @@ public class MyLinkedList {
     }
 
     public void printMyNodes() {
-        StringBuffer myNodes = new StringBuffer("My nodes are ");
-        INode tempNode = head;
-        while (tempNode.getNext() != null) {
+        if (head == null) {
+            System.out.println("linked list is empty");
+        } else {
+            StringBuffer myNodes = new StringBuffer("My nodes are ");
+            INode tempNode = head;
+            while (tempNode.getNext() != null) {
+                myNodes.append(tempNode.getKey());
+                if (!tempNode.equals(tail)) myNodes.append("->");
+                tempNode = tempNode.getNext();
+            }
             myNodes.append(tempNode.getKey());
-            if(!tempNode.equals(tail)) myNodes.append("->");
-            tempNode = tempNode.getNext();
+            System.out.println(myNodes);
         }
-        myNodes.append(tempNode.getKey());
-        System.out.println(myNodes);
     }
 
     public void append(INode newNode) {
-        if(this.head == null) {
+        if (this.head == null) {
             this.head = newNode;
             this.tail = newNode;
-        }
-        else {
+        } else {
             INode tempNode = this.tail;
             tempNode.setNext(newNode);
             this.tail = newNode;
         }
+    }
+
+    public void insertBetween(INode myFirstNode, INode newNode) {
+        INode tempNode = myFirstNode.getNext();
+        myFirstNode.setNext(newNode);
+        newNode.setNext(tempNode);
     }
 }
--- src/main/java/data_structures/MyLinkedListTest.java
+++ src/main/java/data_structures/MyLinkedListTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 
 public class MyLinkedListTest {
     @Test
-    public void When3NumberGivenAddedToTopOfLinkedListTestShouldPass() {
+    public void When2NumberGivenAddedBetweenTwoNumbersTestShouldPass() {
         INode<Integer> myFirstNode = new MyNode<>(56);
         INode<Integer> mySecondNode = new MyNode<>(30);
         INode<Integer> myThirdNode = new MyNode<>(70);
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.append(myFirstNode);
-        myLinkedList.append(mySecondNode);
         myLinkedList.append(myThirdNode);
+        myLinkedList.insertBetween(myFirstNode, mySecondNode);
         myLinkedList.printMyNodes();
         boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
                         && myLinkedList.tail.equals(myThirdNode);
