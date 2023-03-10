9c19078ddea6a351b4f841a01cf752093767f2c4
Akshay4955 <akshayshedge11@gmail.com>
2023-03-10 17:14:50 +0530

    Added: Code for pop method

--- src/main/java/data_structures/MyLinkedList.java
+++ src/main/java/data_structures/MyLinkedList.java
@@ -52,4 +52,8 @@ public class MyLinkedList {
         myFirstNode.setNext(newNode);
         newNode.setNext(tempNode);
     }
+
+    public void pop() {
+        this.head = head.getNext();
+    }
 }
--- src/main/java/data_structures/MyLinkedListTest.java
+++ src/main/java/data_structures/MyLinkedListTest.java
@@ -5,18 +5,17 @@ import org.junit.Test;
 
 public class MyLinkedListTest {
     @Test
-    public void When2NumberGivenAddedBetweenTwoNumbersTestShouldPass() {
+    public void GivenFirstElementWhenDeletedShouldPassLinkedListResult() {
         INode<Integer> myFirstNode = new MyNode<>(56);
         INode<Integer> mySecondNode = new MyNode<>(30);
         INode<Integer> myThirdNode = new MyNode<>(70);
         MyLinkedList myLinkedList = new MyLinkedList();
-        myLinkedList.append(myFirstNode);
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
         myLinkedList.append(myThirdNode);
-        myLinkedList.insertBetween(myFirstNode, mySecondNode);
+        myLinkedList.pop();
         myLinkedList.printMyNodes();
-        boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
-                        && myLinkedList.tail.equals(myThirdNode);
+        boolean result = myLinkedList.head.equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
-
     }
 }
